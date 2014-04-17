8199af6da010fd9acf5af3beec5af4c4b1d15645
Brian Emil Hartz <brianhartz@gmail.com>
2014-04-17 14:06:30 -0600

    solved 1 and 2

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-$(document).on('ready', function() {
-  
-});
+var firstReverse = function(string) {
+    var newString = '';
+    var arr = [];
+    string.split('').map(function(a) {
+        arr.unshift(a)
+    });
+    newString = arr.join('');
+    return newString;
+}
+
+var swapCase = function(string) {
+    // var arr = string.split(' ');
+    // var arr2 = arr.map(function(a) {
+    //     return a.split('').map(function(a, i) {
+    //         return i === 0 ? a.toLowerCase() : a.toUpperCase()
+    //     }).join('');
+    // }).join(' ');
+
+    // return arr2;
+
+    return string.split(' ').map(function(a) {
+        return a.split('').map(function(a, i) {
+            return a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()
+        }).join('')
+    }).join(' ')
+}
--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,24 @@
-describe("Module", function() {
-  it("should do something", function() {
-    expect(true).toEqual(false);
-  });
-});
+// describe("Module", function() {
+//   it("should do something", function() {
+//     expect(true).toEqual(false);
+//   });
+// });
+
+describe("firstReverse takes a single string parameter and returns the string in reverse order", function() {
+    it("should reverse a single string", function() {
+        expect(firstReverse('hello')).toEqual('olleh');
+        expect(firstReverse('yuppers')).toEqual('sreppuy');
+        expect(firstReverse('breakfast')).toEqual('tsafkaerb');
+    })
+})
+
+describe("swapCase that takes a single string parameter and swaps the case of each character. For example: if a string is 'Hello World' the output should be 'hELLO wORLD'. Let numbers and symbols stay the way they are.", function() {
+    it("should lowercase first leffter then cap the rest", function() {
+        expect(swapCase('What Up Doc!')).toEqual('wHAT uP dOC!');
+        expect(swapCase('Super Dooper')).toEqual('sUPER dOOPER');
+        expect(swapCase('Food 100 For Thought.')).toEqual('fOOD 100 fOR tHOUGHT.');
+        expect(swapCase('What!')).toEqual('wHAT!');
+        expect(swapCase('WHAt!')).toEqual('whaT!');
+        expect(swapCase('awAl is! tOO')).toEqual('AWaL IS! Too');
+    })
+})
